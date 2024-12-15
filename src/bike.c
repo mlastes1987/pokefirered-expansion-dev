38bfa154b0f37b24d5183f2ba54b49a5f70c2768
MARK\mlast <mlastes1987@gmail.com>
2024-12-14 21:49:36 -0500

    Update: Removed running inside after crash after receiving Running Shoes.

--- src/bike.c
+++ src/bike.c
@@ -252,10 +252,12 @@ bool8 RS_IsRunningDisallowed(u8 r0)
 
 bool32 IsRunningDisallowed(u8 metatileBehavior)
 {
-    if (IsRunningDisallowed(metatileBehavior) == TRUE)
+    if (!gMapHeader.allowRunning)
         return TRUE;
-    else
+    if (MetatileBehaviorForbidsBiking(metatileBehavior) != TRUE)
         return FALSE;
+    else
+        return TRUE;
 }
 
 static bool8 MetatileBehaviorForbidsBiking(u8 metatileBehavior)
